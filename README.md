1ddd6b00c9279c0dac5ed185c32c2d2ccc709380
Tsuunen <laforgueremi@gmail.com>
2022-12-28 11:42:29 +0100

    v0.3

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-# Simili Finder
+# Simili Finder (v0.3)
 
 Find file with the same name
 
@@ -10,5 +10,21 @@ sudo snap install simili-finder --devmode
 
 To update :
 ```
-sudo snap refresh simili-finder --devmode #didn't try but maybe
+sudo snap refresh simili-finder --devmode 
 ```
+
+Syntax :
+  To find file with the same name :
+  ```
+  simili-finder <folder path> <output file name>
+  ```
+
+  To find file with the same x same character :
+  ```
+  simili-finder <folder path> <output file name> <number of character>
+  ```
+
+  To find file with the same word in it:
+  ```
+  simili-finder <folder path> <output file name> word
+  ```
--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def doublon():
   {back.join(values)}
                              """)
         print(f"{output_file_name}.txt created")
-        print(f"Succesfull operation")
+        print("Succesfull operation")
 
     except IndexError:
         print("""error : argument(s) missing
@@ -79,8 +79,54 @@ def start_doublon():
     except IndexError:
         print("error : argument(s) missing \n \"simili-finder <folder path> <output file name> <num char>\"")
 
+def word_doublon():
+    file = {}
+
+    try:
+        chemin = argv[1]
+        output_file_name = argv[2]
+
+        for (repertoire, sousRepertoire, fichiers) in os.walk(chemin):
+            print(repertoire)
+            for i in fichiers:
+                der_indice = -1
+                mots = []
+                for k in range(len(i)):
+                    if i[k] in [" ", "/", "-", "_", ",", ".", "'", "(", ")"]:
+                        mots.append(i[der_indice + 1:k])
+                        der_indice = k
+                if len(mots) == 0:
+                    mots.append(i)
+                for j in mots:
+                    try:
+                        if file[j]:
+                            file[j].append(repertoire)
+                    except:
+                        file[j] = [repertoire]
+
+        back = "\n  "
+        if os.path.exists(f"{output_file_name}.txt"):
+            os.remove(f"{output_file_name}.txt")
+            print(f"{output_file_name}.txt removed")
+
+        for keys, values in file.items():
+            if len(values) > 1:
+                with open(f"{output_file_name}.txt", "a") as output:
+                    output.write(f"""
+{keys} :
+  {back.join(values)}
+                                 """)
+        print(f"{output_file_name}.txt created")
+        print("Succesfull operation")
+    except IndexError:
+        print("error : argument(s) missing \n \"simili-finder <folder path> <output file name> word")
+
 try:
     arg = argv[3]
-    start_doublon()
+    try:
+        arg = int(arg)
+        start_doublon()
+    except:
+        word_doublon()
 except:
     doublon()
