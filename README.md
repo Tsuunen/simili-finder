feb0039606b0e0d09721989f4fd971ee44fa2bef
Tsuunen <laforgueremi@gmail.com>
2022-12-30 11:43:15 +0100

    v0.5

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-# Simili Finder (v0.4.1)
+# Simili Finder (v0.5)
 
 Find file with the same name
 
@@ -13,24 +13,3 @@ To update :
 sudo snap refresh simili-finder --devmode 
 ```
 
-Syntax :
-
-  * To find file with the same name :
-  ```
-  simili-finder scan <folder path> <output file name>
-  ```
-
-  * To find file with the same x same character :
-  ```
-  simili-finder scan <folder path> <output file name> <number of character>
-  ```
-
-  * To find file with the same word in it :
-  ```
-  simili-finder scan <folder path> <output file name> word
-  ```
-
-  * To find a specific file :
-  ```
-  simili-finder find <folder path> <output file name> <file name>
-  ```
--- main.py
+++ main.py
@@ -1,48 +1,100 @@
 #!/bin/python3
 
 import os
-from sys import argv
+import time
 
-def doublon(chemin):
-    file = {}
 
-    for (repertoire, sousRepertoire, fichiers) in os.walk(chemin):
-        print(repertoire)
-        for i in fichiers:
+def help():
+    print("""
+Simili-finder v0.5
+
+commands :
+    scan : find file with some similarity
+        -o : set the output file name (default = "simili-finder_scan_[timezone].txt")
+        -d : set the folder to scan (default = current folder)
+        -a : set additional parameter :
+                "word" to shearch file with the same word in it
+                <number (int)> to shearch file who have the same x first character
+
+    find : find specific file
+        -o : set the output file name (default = "simili-finder_find_[timezone].txt")
+        -d : set the folder to look for the file (default = current folder)
+        -f : name of the file (mandatory)
+
+    help : see the command list
+            
+        """)
+
+
+def get_parameter(parameter: list) -> dict:
+    options = {f"{parameter[0]}": "oui"}
+
+    for opt in range(len(parameter)):
+        if parameter[0] == "scan":
+            if parameter[opt] == "-o":
+                options['output_file_name'] = parameter[opt + 1]
+            elif parameter[opt] == "-d":
+                options["directory"] = parameter[opt + 1]
+            elif parameter[opt] == "-a":
+                options["add_par"] = parameter[opt + 1]
+
+        elif parameter[0] == "find":
+            if parameter[opt] == "-o":
+                options["output_file_name"] = parameter[opt + 1]
+            elif parameter[opt] == "-d":
+                options["directory"] = parameter[opt + 1]
+            elif parameter[opt] == "-f":
+                options["file"] = parameter[opt + 1]
+
+        else:
+            print("Simili-finder: invalid syntax")
+            return options
+    return options
+
+
+def doublon(path):
+    result = {}
+    delimiter = os.pathsep
+
+    for (directory, o, files) in os.walk(path):
+        print(directory)
+        for i in files:
             name = os.path.splitext(i)
-            try:
-                if file[name[0]]:
-                    file[name[0]].append(repertoire + "/" + i)
-            except:
-                file[name[0]] = [repertoire + "/" + i]
-    return file
+        try:
+            if result[name[0]]:
+                result[name[0]].append(directory + delimiter + i)
+        except:
+            result[name[0]] = [directory + delimiter + i]
+    return result
 
 
-def start_doublon(chemin, nbr_char):
-    file = {}
+def start_doublon(path: str, nbr_char: int):
+    result = {}
+    delimiter = os.path.sep
 
-    for (repertoire, sousRepertoire, fichiers) in os.walk(chemin):
-        print(repertoire)
-        for i in fichiers:
+    for (directory, o, files) in os.walk(path):
+        print(directory)
+        for i in files:
             name = os.path.splitext(i)
             if len(name[0]) > nbr_char:
                 name = name[0][:nbr_char]
             else:
                 name = name[0]
             try:
-                if file[name]:
-                    file[name].append(repertoire + "/" + i)
+                if result[name]:
+                    result[name].append(directory + delimiter + i)
             except:
-                file[name] = [repertoire + "/" + i]
-    return file
+                result[name] = [directory + delimiter + i]
+    return result
 
 
-def word_doublon(chemin):
-    file = {}
+def word_doublon(path):
+    result = {}
+    delimiter = os.pathsep
 
-    for (repertoire, sousRepertoire, fichiers) in os.walk(chemin):
-        print(repertoire)
-        for i in fichiers:
+    for (directory, o, files) in os.walk(path):
+        print(directory)
+        for i in files:
             der_indice = -1
             mots = []
             for k in range(len(i)):
@@ -53,78 +105,107 @@ def word_doublon(chemin):
                 mots.append(i)
             for j in mots:
                 try:
-                    if file[j]:
-                        file[j].append(repertoire + "/" + i)
+                    if result[j]:
+                        result[j].append(directory + delimiter + i)
                 except:
-                    file[j] = [repertoire + "/" + i]
-    return file
+                    result[j] = [directory + delimiter + i]
+    return result
 
-def find(chemin, file_name):
-    file = {}
 
-    for (repertoire, sousRepertoire, fichiers) in os.walk(chemin):
-        print(repertoire)
-        for i in fichiers:
+def find(path, file_name):
+    result = {}
+    delimiter = os.pathsep
+
+    for (directory, o, files) in os.walk(path):
+        print(directory)
+        for i in files:
             name = os.path.splitext(i)
             if name[0] == file_name:
                 try:
-                    if file[name[0]]:
-                        file[name[0]].append(repertoire + "/" + i)
+                    if result[name[0]]:
+                        result[name[0]].append(directory + delimiter + i)
                 except:
-                    file[name[0]] = [repertoire + "/" + i]
-    if len(file) == 0:
-        file["Not Found"] = ["File not found", "See more on : github.com/Tsuunen/simili-finder"]
-    return file
+                    result[name[0]] = [directory + delimiter + i]
+
+    return result
 
 
+help()
 
-try:
-    action = argv[1]
-    chemin = argv[2]
-    output_file_name = argv[3]
+while True:
+    command = input(" > ")
+    parameter = command.split(" ")
 
-    if action == "scan":
-        try:
-            arg = argv[4]
-            try:
-                arg = int(arg)
-                file = start_doublon(chemin, arg)
-            except:
-                file = word_doublon(chemin)
-        except:
-            file = doublon(chemin)
-    elif action == "find":
-        try:
-            file_name = argv[4]
-            file = find(chemin, file_name)
-        except:
-            print("error : file name not providen")
+    if parameter[0] == "help":
+        help()
     else:
-        print("action doesn't exists")
-        exit()
+        options = get_parameter(parameter)
 
-    back = "\n  "
-    if os.path.exists(f"{output_file_name}.txt"):
-        os.remove(f"{output_file_name}.txt")
-        print(f"{output_file_name}.txt removed")
+        if parameter[0] == "scan":
+            try:
+                output_file_name = options["output_file_name"]
+            except:
+                t = time.localtime()
+                output_file_name = f"simili-finder_scan_{t[3]}:{t[4]}:{t[5]}"
+            try:
+                if not os.path.exists(options["directory"]):
+                    print("simili-finder: directory not found")
+                    continue
+                directory = options["directory"]
+            except:     
+                directory = "."
+            try:
+                add_option = options["add_par"]
+            except:
+                add_option = False
 
-    for keys, values in file.items():
-        if action != "find":
-            if len(values) > 1:
-                with open(f"{output_file_name}.txt", "a") as out:
-                    out.write(f"""
-{keys}:
-  {back.join(values)}
-                        """)
-        else:
-            with open(f"{output_file_name}.txt", "a") as out:
-                out.write(f"""
+            print(add_option)
+            if not add_option:
+                result = doublon(directory)
+            else:
+                try:
+                    add_option = int(add_option)
+                    result = start_doublon(directory, add_option)
+                except:
+                    result = word_doublon(directory)
+        
+        elif parameter[0] == "find":
+            try:
+                output_file_name = options["output_file_name"]
+            except:
+                t = time.localtime()
+                output_file_name = f"simili-finder_find_{t[3]}:{t[4]}:{t[5]}"
+            try:
+                directory = options["directory"]
+            except:
+                directory = "."
+            try:
+                file = options["file"]
+            except:
+                print("Simili-finder: file name mandatory")
+                continue
+            result = find(directory, file)
+        
+    if len(result) != 0:
+        back = "\n  "
+        if os.path.exists(f"{output_file_name}.txt"):
+            os.remove(f"{output_file_name}.txt")
+            print(f"{output_file_name}.txt")
+
+        for keys, values in result.items():
+            if parameter[0] != "find":
+                if len(values) > 1:
+                    with open(f"{output_file_name}.txt", "a") as out:
+                        out.write(f"""
 {keys} :
   {back.join(values)}
-                          """)
-    print(f"{output_file_name}.txt created")
-    print("Succesfull operation")
-
-except:
-    print("error : see more info on github.com/Tsuunen/simili-finder")
+                                  """)
+            else:
+                with open(f"{output_file_name}.txt", "a") as out:
+                    out.write(f"""
+{keys} : 
+  {back.join(values)}
+                              """)
+        print(f"{output_file_name}.txt created")
+        print("Succesfull operation")
 
